463d916bf68c125dca1f0239c4d249be20efdc3e
Ihssane Koukou <ihssane.ke@gmail.com>
2024-10-17 12:42:21 +0100

    Refactor timer component and add keyboard shortcuts

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,279 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { LuMoveDown, LuMoveLeft, LuMoveRight, LuMoveUp } from "react-icons/lu";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isDanger, setIsDanger] = useState(false);
+  const [isCountDown, setIsCountDown] = useState(false);
+  const [editingPart, setEditingPart] = useState("");
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const pad = (n: number) => (n < 10 ? `0${n}` : n);
+
+  useEffect(() => {
+    if (isActive) {
+      const interval = setInterval(() => {
+        if (!isCountDown) {
+          timerUp();
+        } else {
+          timerDown();
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  });
+
+  useEffect(() => {
+    const detectPressedKey = (e: KeyboardEvent) => {
+      console.log(e.key);
+      if (e.key === " ") {
+        e.preventDefault();
+        if (isActive) stopTimer();
+        else startTimer();
+      }
+      if (e.key === "ArrowLeft") {
+        setEditingPart("minutes");
+        if (isActive) stopTimer();
+      }
+      if (e.key === "ArrowRight") {
+        setEditingPart("seconds");
+        stopTimer();
+      }
+      if (e.key === "ArrowUp") {
+        if (isActive) stopTimer();
+        timerUp();
+      }
+      if (e.key === "ArrowDown") {
+        if (isActive) stopTimer();
+        timerDown();
+      }
+      if (e.key === "f" || e.key === "F") fullScreen();
+      if (e.key === "r" || e.key === "R") {
+        if (isActive) stopTimer();
+        resetTimer();
+      }
+      if (e.key === "s" || e.key === "S") {
+        setIsCountDown(!isCountDown);
+      }
+      if (e.key === "d" || e.key === "D") {
+        setIsDanger(!isDanger);
+      }
+    };
+    document.addEventListener("keydown", detectPressedKey, true);
+    return () => {
+      document.removeEventListener("keydown", detectPressedKey, true);
+    };
+  });
+
+  const fullScreen = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      setIsFullScreen(false);
+    } else {
+      document.documentElement.requestFullscreen();
+      setIsFullScreen(true);
+    }
+  };
+
+  const resetTimer = () => {
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const timerUp = () => {
+    if (editingPart === "minutes") {
+      setMinutes((prev) => prev + 1);
+      return;
+    } else {
+      if (seconds === 59) {
+        setMinutes((prev) => prev + 1);
+        setSeconds(0);
+        return;
+      }
+      setSeconds((prev) => prev + 1);
+    }
+  };
+
+  const timerDown = () => {
+    if (editingPart === "minutes") {
+      if (minutes === 0) {
+        if (isActive) stopTimer();
+        return;
+      }
+      setMinutes((prev) => prev - 1);
+      return;
+    } else {
+      if (seconds === 0) {
+        if (minutes === 0) {
+          setIsActive(false);
+          return;
+        }
+        setMinutes((prev) => prev - 1);
+        setSeconds(59);
+        return;
+      }
+      setSeconds((prev) => prev - 1);
+    }
+  };
+
+  const startTimer = () => {
+    setEditingPart("");
+    setIsActive(true);
+  };
+  const stopTimer = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <div
+      style={{ fontSize: "30vw" }}
+      className={`flex flex-col items-center justify-center  space-y-5  font-extrabold min-h-svh`}
+    >
+      <div
+        className={`${
+          isDanger ? " text-red-500 focus:outline focus:ring" : ""
+        }`}
+      >
+        <span
+          onClick={() => {
+            if (isActive) stopTimer();
+            setEditingPart("minutes");
+          }}
+          className={`${
+            editingPart === "minutes" && !isActive ? "pulsing-element" : ""
+          } font-mono`}
+        >
+          {pad(minutes)}
+        </span>
+        <span className="font-mono">:</span>
+        <span
+          onClick={() => {
+            if (isActive) stopTimer();
+            setEditingPart("seconds");
+          }}
+          className={`${
+            editingPart === "seconds" && !isActive ? "pulsing-element" : ""
+          } font-mono`}
+        >
+          {pad(seconds)}
+        </span>
+      </div>
+      <div className="absolute bottom-10">
+        <div className="flex flex-col items-start justify-center space-y-2">
+          <div className="flex items-center justify-center space-x-1 ">
+            <button
+              onClick={fullScreen}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              F
+            </button>{" "}
+            <span className="text-xs opacity-20 font-extralight flex items-center justify-center">
+              - {isFullScreen ? "Exit Fullscreen" : "Fullscreen"}
+            </span>
+          </div>
+          <div className="flex space-x-2">
+            <button
+              onClick={() => {
+                setEditingPart("minutes");
+                if (isActive) stopTimer();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              <LuMoveLeft />
+            </button>
+            <button
+              onClick={() => {
+                setEditingPart("seconds");
+                stopTimer();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              <LuMoveRight />
+            </button>
+            <button
+              onClick={() => {
+                if (isActive) stopTimer();
+                timerUp();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              <LuMoveUp />
+            </button>
+            <button
+              onClick={() => {
+                if (isActive) stopTimer();
+                timerDown();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              <LuMoveDown />
+            </button>
+            <span className="text-xs opacity-20 font-extralight flex items-center justify-center">
+              {" "}
+              -{" "}
+              {editingPart === "minutes"
+                ? "Up / Down to edit Minutes"
+                : "Up / Down to Edit Seconds"}
+            </span>
+          </div>
+          <div className="flex items-center justify-center space-x-1 ">
+            <button
+              onClick={() => {
+                if (isActive) stopTimer();
+                resetTimer();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              R
+            </button>
+            <span className="text-xs opacity-20  font-extralight flex items-center justify-center">
+              - Reset Timer
+            </span>
+          </div>
+          <div className="flex items-center justify-center space-x-1 ">
+            <button
+              onClick={() => setIsCountDown(!isCountDown)}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              S
+            </button>
+            <span className="text-xs opacity-20 font-extralight flex items-center justify-center">
+              - {isCountDown ? "Countdown" : "Stopwatch"}
+            </span>
+          </div>
+          <div className="flex items-center justify-center space-x-1 ">
+            <button
+              onClick={() => {
+                setIsDanger(!isDanger);
+              }}
+              className="text-xs border font-extralight py-1 px-2 bg-red-500 rounded-md opacity-20 hover:opacity-60"
+            >
+              D
+            </button>
+            <span className="text-xs opacity-20 font-extralight flex items-center justify-center">
+              - {isDanger ? "In Danger" : "No Danger"}
+            </span>
+          </div>
+          <div className="flex items-center justify-center space-x-1 ">
+            <button
+              onClick={() => {
+                if (isActive) stopTimer();
+                else startTimer();
+              }}
+              className="text-xs border font-extralight py-1 px-2 rounded-md opacity-20 hover:opacity-60"
+            >
+              Space
+            </button>
+            <span className="text-xs opacity-20 font-extralight flex items-center justify-center">
+              - {isActive ? "Stop" : "Start"}
+            </span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
